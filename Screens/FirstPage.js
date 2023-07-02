0bb44e873d79328fb8326712baca182a9e9c8fa0
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-07-02 14:05:20 +0200

    set inwinner state and correct,reverse the flatlist

--- Screens/FirstPage.js
+++ Screens/FirstPage.js
@@ -10,14 +10,15 @@ export default function FirstPage() {
     const [logNum, setLogNum] = useState([]);
     const [currentNum, setCurrentNum] = useState("");
 
-
+  
     function addNum(num) {
-        if (opponentGuess === num) {
-            setIsWinner(true);
-            console.log("winnerrrrrrr")
-        } else {
+        if (!isWinner) {
             setLogNum([...logNum, { id: uuid.v4(), num: num }]);
-            setCurrentNum(num)
+            setCurrentNum(num);
+            if (opponentGuess == num) {
+                setIsWinner(true);
+                console.log("winnerrrrrrr")
+            }
         }
 
     }
@@ -46,14 +47,14 @@ export default function FirstPage() {
                 <Guess add={addNum} firtUpdate={firtUpdateCurrentNum} />
             </View >
             <View style={styles.flatList}>
-                <FlatList 
-                data={logNum}
-                renderItem={({item,index})=>(
-                    <View  key={item.id} style={styles.flatlistItem}>
-                        <Text>#{index}</Text>
-                        <Text >Opponent's Guess: {item.num}</Text>
-                    </View>
-                )} />
+                <FlatList
+                    data={[...logNum].reverse()}
+                    renderItem={({ item, index }) => (
+                        <View key={item.id} style={styles.flatlistItem}>
+                            <Text>#{index+1}</Text>
+                            <Text >Opponent's Guess: {item.num}</Text>
+                        </View>
+                    )} />
             </View>
         </View>
 
@@ -70,52 +71,52 @@ const styles = StyleSheet.create({
     },
     header: {
         width: "80%",
-        height:"7%",
+        height: "7%",
         borderWidth: 2,
         borderColor: "white",
         justifyContent: "center",
         alignItems: "center",
         marginTop: 100,
     },
-    headerText:{
+    headerText: {
         color: "white",
-        fontSize:30,
-        fontWeight:600
+        fontSize: 30,
+        fontWeight: 600
     },
     showNum: {
-        width:"70%",
+        width: "70%",
         borderWidth: 2,
         borderColor: "yellow",
-        marginTop:"3%",
-        justifyContent:"center",
-        alignItems:"center",
+        marginTop: "3%",
+        justifyContent: "center",
+        alignItems: "center",
         flex: 1
     },
-    showNumText:{
+    showNumText: {
         color: "yellow",
-        fontSize:30,
-        fontWeight:600
+        fontSize: 30,
+        fontWeight: 600
     },
     chooseNav: {
-        width:"70%",
+        width: "70%",
         flex: 1,
-        marginTop:"10%"
+        marginTop: "10%"
     },
     flatList: {
-        width:"75%",
+        width: "75%",
         flex: 3,
-        marginBottom:"20%",
-        marginTop:"5%"
+        marginBottom: "20%",
+        marginTop: "5%"
     },
-    flatlistItem:{
-        backgroundColor:"yellow",
-        borderRadius:7,
-        marginBottom:10,
-        height:40,
-        flexDirection:"row",
-        alignItems:"center",
-        justifyContent:"space-between",
-        paddingHorizontal:10
+    flatlistItem: {
+        backgroundColor: "yellow",
+        borderRadius: 7,
+        marginBottom: 10,
+        height: 40,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 10
     }
 
 })
